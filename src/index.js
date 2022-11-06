87ca68697535f382ecea77b8e727af3d0cffbb72
gopimanickam132001 <113223786+gopimanickam132001@users.noreply.github.com>
2022-11-06 09:21:04 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from "react";
-import { ReactDOM } from "react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+// const port = 5000 || process.env.PORT;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+
 );
 
+// App.listen(port, () => console.log(`Server is running on port ${port}`));
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
