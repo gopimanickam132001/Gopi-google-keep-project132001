02a65b29a912108adde0d83b54ea8d08ae3da35e
gopimanickam132001 <113223786+gopimanickam132001@users.noreply.github.com>
2022-11-06 05:35:12 -0800

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer(props) {
+  return (
+    <div className="footer">
+      <p>@ Programmig with coderunners</p>
+    </div>
+  );
+}
+
+export default Footer;
+
