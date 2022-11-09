68884b087bff25ae6372c91752df1a1e7e2037eb
gopimanickam132001 <113223786+gopimanickam132001@users.noreply.github.com>
2022-11-09 00:40:34 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles.css'
+import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// const port = 5000 || process.env.PORT;
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-
 );
-
-// App.listen(port, () => console.log(`Server is running on port ${port}`));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
