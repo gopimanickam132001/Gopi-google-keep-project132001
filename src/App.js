410c7b56aac4409537f7eb0bbc2c8ba0b2b88761
gopimanickam132001 <113223786+gopimanickam132001@users.noreply.github.com>
2022-11-09 00:38:12 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,65 +1,13 @@
-import React from 'react';
-import Header from "./components/Header";
-import CreateArea from './components/CreateArea';
-import Note from './components/Note';
-import Count from "./components/Count";
-import Footer from "./components/Footer";
-import './styles.css'
-import {useState} from 'react';
 
+//components
+import Home from './components/Home';
+import DataProvider from './context/DataProvider';
 
-function App(props) {
-  const [notes, setNotes] = useState([]);
-
-  function addNote(newNote){
-    setNotes((prevValue)=>{
-      return [...prevValue, newNote];
-    });
-  }
-
-  function deleteNotes(id) {
-    setNotes((preValue) => {
-      localStorage.removeItem(id+1);
-      return [...preValue.filter((note, index) => index !== id)];
-    });
-  }
-
-  function allStorage() {
-    var keys = Object.keys(localStorage),
-    totalItems = keys.length;
-    return totalItems;
-  }
-  
-  var indexVal = allStorage();
-  let values = localStorage.getItem(indexVal);
-  // const noteValues = JSON.parse(values);
-
+function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Count
-        count={
-          notes.length === 0
-            ? "No records"
-            : `Showing ${notes.length} Note(s)`
-        }
-      />
-      <CreateArea onAdd={addNote} />
-      
-      {notes.map((note, index) => (
-        <Note
-          key={index}
-          id={index}
-          localContent = {values}
-          title={note.title}
-          content={note.content}
-          onDelete={deleteNotes}  
-        />
-      ))}
-
-      <Footer />
-      
-    </div>
+    <DataProvider>
+      <Home />
+    </DataProvider>
   );
 }
 
